6cd5877ba46c678a37bd56d939a3f302bb6a7390
2754github <fkesys@gmail.com>
2020-03-24 21:03:23 +0900

    AC

---
+++ ABC156B.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+
+
+def Base_10_to_n(X, n):
+    if (int(X/n)):
+        return Base_10_to_n(int(X/n), n)+str(X % n)
+    return str(X % n)
+
+
+print(len(str(Base_10_to_n(N, K))))
