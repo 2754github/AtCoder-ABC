9315e1bd0c6754bccb074e51aee88429300aa8d4
2754github <fkesys@gmail.com>
2020-03-24 22:20:27 +0900

    FixしたけどWA．助けて．

--- ABC157C.py
+++ ABC157C.py
@@ -3,10 +3,10 @@ N, M = map(int, input().split())
 A = ["*"]*N
 for _ in range(M):
     s, c = map(int, input().split())
-    if A[s - 1] == -1:
+    if A[s - 1] == "*":
         A[s - 1] = str(c)
     else:
-        if A[s - 1] == c:
+        if A[s - 1] == str(c):
             pass
         else:
             print(-1)
@@ -16,12 +16,12 @@ A_replace = [a.replace("*", "0") for a in A]
 if N == 1:
     print(A_replace[0])
 elif N == 2:
-    if A_replace[0] == 0:
+    if A_replace[0] == "0":
         print(-1)
     else:
-        print("".join(A_replace[0]))
+        print("".join(A_replace))
 elif N == 3:
-    if A_replace[0] == 0:
+    if A_replace[0] == "0":
         print(-1)
     else:
-        print("".join(A_replace[0]))
+        print("".join(A_replace))
