5f3ae7e970995ccc629a08d117075878116e1b02
2754github <fkesys@gmail.com>
2020-03-24 22:07:39 +0900

    Fix

--- ABC157C.py
+++ ABC157C.py
@@ -16,7 +16,12 @@ A_replace = [a.replace("*", "0") for a in A]
 if N == 1:
     print(A_replace[0])
 elif N == 2:
-    if A_replace == 0:
+    if A_replace[0] == 0:
+        print(-1)
+    else:
+        print("".join(A_replace[0]))
+elif N == 2:
+    if A_replace[0] == 0:
         print(-1)
     else:
         print("".join(A_replace[0]))
